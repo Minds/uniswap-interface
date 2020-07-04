5c0d3d053ef698df2ecb1f8236a95ee30be1bb7d
Mark Harding <mark@kramnorth.com>
2020-07-04 12:36:59 +0100

    chore(constant): add MINDS token

--- src/constants/tokens/mainnet.ts
+++ src/constants/tokens/mainnet.ts
@@ -128,5 +128,6 @@ export default [
   new Token(ChainId.MAINNET, '0x09fE5f0236F0Ea5D930197DCE254d77B04128075', 18, 'WCK', 'Wrapped CryptoKitties'),
   new Token(ChainId.MAINNET, '0xB4272071eCAdd69d933AdcD19cA99fe80664fc08', 18, 'XCHF', 'CryptoFranc'),
   new Token(ChainId.MAINNET, '0x0f7F961648aE6Db43C75663aC7E5414Eb79b5704', 18, 'XIO', 'XIO Network'),
+  new Token(ChainId.MAINNET, '0xb26631c6dda06ad89b93c71400d25692de89c068', 18, 'MINDS', 'Minds Token'),
   new Token(ChainId.MAINNET, '0xE41d2489571d322189246DaFA5ebDe1F4699F498', 18, 'ZRX', '0x Protocol Token')
 ]
